8f33822a95a2590e7d5a7d5a97d465f3bb6b4987
mcreng <tsehonam@gmail.com>
2019-12-05 21:17:35 +0800

    feat: yolo api now takes redis id instead of data uri

--- yolov3/server.py
+++ yolov3/server.py
@@ -6,11 +6,13 @@ from gevent.pywsgi import WSGIServer
 import cv2
 import numpy as np
 import torch
+from redis import Redis
 
 from datauri import DataURI
 from detect import detect
 
 app = Flask(__name__)
+r = Redis(host='serverless-redis-master.openfaas-fn', password='admin')
 
 
 @app.route('/', methods=['POST'])
@@ -19,7 +21,7 @@ def get_pred():
     POST /
 
     Params:
-        images (list): List of data uri specified images.
+        images (list): List of IDs to points to images in Redis.
 
     Returns:
         Prediction in list of object, each object specifies the the two corners of the bounding box, the confidence level and the prediction.
@@ -30,7 +32,10 @@ def get_pred():
     else:
         return jsonify([])
 
-    # Convert strings to DataURI
+    # Convert keys to DataURI through redis
+    images = list(map(lambda s: r.get(s).decode(), images))
+
+    # Convert strings to DataURIs
     images = list(map(lambda b: DataURI(b), images))
 
     # Filter out non-images
